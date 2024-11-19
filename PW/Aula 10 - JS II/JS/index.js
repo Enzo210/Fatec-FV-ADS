b86df9ade465dd9f13650b7c26a80821fceadf8b
Enzo210 <105013823+Enzo210@users.noreply.github.com>
2024-11-19 12:05:27 -0300

    0588

--- PW/Aula 10 - JS II/JS/index.js
+++ PW/Aula 10 - JS II/JS/index.js
@@ -1,41 +1,94 @@
-//selecionamos os elementos do HTML
-const titulo = document.getElementById('titulo')
-const texto = document.querySelector('.texto')
-const botao = document.getElementById('botao')
-console.log(titulo,texto,botao)
+// selecionamos os elementos do HTML
+const titulo = document.getElementById('titulo')  //selecionamos elementos com id
+const texto = document.querySelector('.texto') //selecionamos elementos
+const botao = document.getElementById('botao') 
+console.log(titulo, texto, botao)
 
 //selecionando elementos que não existe
 const texto1 = document.querySelector('texto1')
 const botao1 = document.getElementById('botao1')
-console.log(texto1,botao1)
+console.log(texto1, botao1)
 
-botao.addEventListener( 'click',()=>{
+// inserindo um evento no botao
+botao.addEventListener('click',()=>{
     alert('Sou um evento externo')
 })
 
-//manipulando conteudos e Estilos
+
+//Manipulando conteudos e Estilos 
 
 //alterar o conteudo textual
 titulo.textContent = 'Aula com DOM em JS'
-texto.innerHTML = "<strong> Texto adicionando pelosdfgsdfgsdfgsdfgsdfg o JS</strong>"
+
+texto.innerHTML = "<strong>Texto adicionado pelo o JS</strong>"
 
 //alterando estilos
 
-titulo.style.color = "red"
+titulo.style.color = "blue"
 
 texto.style.fontSize = "20px"
 
-
-//MANIPULANDO ELEMENTOs
+//MANIPULANDO ELEMENTOS
 //criando um elemento
 const novoParagrafo = document.createElement('p')
-//adicionando conteudo ao novo elemento
-novoParagrafo.textContent = "Meu novo paasdfasdfragrafo"
-//adicionando um novo elemento no body
+
+//adicionando conteúdo no novo elemento
+novoParagrafo.textContent = "Meu novo paragrafo"
+
+//adicionando um novo elemento no Body
 document.body.appendChild(novoParagrafo)
 
-const div = document.getElementsByClassName('textoinicio')
+
+const div = document.getElementById('textoInicio')
 console.log(div)
+
 div.appendChild(novoParagrafo)
-//alterado um elemento
+
+//alterando um elemento
+
 //removendo um elemento
+
+// exemplo para MouseOver e MouseOut
+let meuDIV = document.getElementById('meuDIV')
+
+meuDIV.addEventListener('mouseover',()=>{
+    meuDIV.style.backgroundColor = 'blue'
+})
+
+meuDIV.addEventListener('mouseout', ()=>{
+    meuDIV.style.backgroundColor = 'brown'
+})
+
+let entrada = document.getElementById('entrada')
+
+//exemplo keyup: quando a tecla é liberada
+entrada.addEventListener('keyup', (e)=>{
+    console.log('Tecla liberada: '+e.key)
+})
+
+//exemplo keydown: quando a tecla é pressionada
+entrada.addEventListener('keydown', (e)=>{
+    alert('Tecla pressionada: '+e.key)
+})
+
+//exemplo change: quando o valor do input é alterado
+
+let entrada2 = document.getElementById('entrada2')
+
+entrada2.addEventListener('change', (e)=>{
+    alert('Valor alterado: '+e.target.value)
+})
+
+
+//exemplo submit: quando o formulário é submetido
+let meuForm = document.getElementById('meuForm')
+
+meuForm.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    let ent_nome = document.getElementById('ent_nome')
+    alert('O nome é: '+ ent_nome.value)
+    alert('Formulário submetido')
+})
+
+
+
