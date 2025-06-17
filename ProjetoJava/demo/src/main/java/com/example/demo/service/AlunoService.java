9d143399f3872ae44ec88711e4bfb5b01007e241
Enzo Sampaio <105013823+Enzo210@users.noreply.github.com>
2025-06-17 09:57:25 -0300

    Update

---
+++ ProjetoJava/demo/src/main/java/com/example/demo/service/AlunoService.java
@@ -0,0 +1,88 @@
+package com.example.demo.service;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Aluno;
+import com.example.demo.repository.AlunoRepository;
+
+@Service
+public class AlunoService implements AlunoRepository{
+
+    
+    AlunoRepository alunoRepository;
+    
+    @Override
+    public <S extends Aluno> S save(S entity) {
+        // TODO Auto-generated method stub
+       return alunoRepository.save(entity);
+    }
+
+    @Override
+    public <S extends Aluno> Iterable<S> saveAll(Iterable<S> entities) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'saveAll'");
+    }
+
+    @Override
+    public Optional<Aluno> findById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    }
+
+    @Override
+    public boolean existsById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'existsById'");
+    }
+
+    @Override
+    public Iterable<Aluno> findAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+
+    @Override
+    public Iterable<Aluno> findAllById(Iterable<Long> ids) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAllById'");
+    }
+
+    @Override
+    public long count() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'count'");
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteById'");
+    }
+
+    @Override
+    public void delete(Aluno entity) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
+
+    @Override
+    public void deleteAllById(Iterable<? extends Long> ids) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteAllById'");
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends Aluno> entities) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteAll'");
+    }
+
+    @Override
+    public void deleteAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteAll'");
+    }
+    
+}
